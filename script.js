d5da2dd635570233222968618f8c64dcc1fd187f
Aaron <aaronkh19@gmail.com>
2020-04-23 17:35:52 -0600

    got the current date to display as part of the header

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+$(document).ready(function () {
+  //setting the current date in the global variables, should change depending on the current date
+  var dateNow = moment().format("MMMM Do YYYY");
 
+  //Using the "currentDay" id to display the date onto the page in the JumbTron as part of the header
+  var $plannerHeading = $("#currentDay");
+  $plannerHeading.text(dateNow);
+});
