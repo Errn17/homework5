c6ab833d9e334685bbac2fd5d8457e58fdc2c863
Aaron <aaronkh19@gmail.com>
2020-04-26 20:24:32 -0600

    kinda restarted..had a hard time creating everything in java, so I used bootstarp to get the basic layout of the page

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ $(document).ready(function () {
   var dateNow = moment().format("MMMM Do YYYY");
 
   //Using the "currentDay" id to display the date onto the page in the JumbTron as part of the header
-  var $plannerHeading = $("#currentDay");
-  $plannerHeading.text(dateNow);
+  var plannerHeading = $("#currentDay");
+  plannerHeading.text(dateNow);
+
+  for (var i = 9; i <= 17; i++) {
+    var rowDiv = $("<div>");
+
+    rowDiv.class("row");
+    rowDiv.class("hour");
+    rowDiv.class("timeblock");
+    rowDiv.attr(i, "row", "hour", "timeblock");
+
+    console.log(rowDiv);
+  }
 });
